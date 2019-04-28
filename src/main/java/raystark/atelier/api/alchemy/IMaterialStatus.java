1a821ee012264c04806b485ec9b4a44df31a46bc
RayStarkMC <raystarkmc@gmail.com>
2019-04-28 21:38:52 +0900

    錬金素材の型を表すインターフェース追加

--- src/main/java/raystark/atelier/api/alchemy/IMaterialStatus.java
+++ src/main/java/raystark/atelier/api/alchemy/IMaterialStatus.java
@@ -1,9 +1,17 @@
 package raystark.atelier.api.alchemy;
 
 
+import net.minecraft.item.Item;
+
 /**
  * 錬金素材の性能を表すインターフェース
  */
 public interface IMaterialStatus extends Quality, ElementOwner {
+    /**
+     * 錬金材料として使われたアイテムを返す.
+     * @return 錬金材料
+     */
+    Item getItem();
+
     // TODO 潜在能力について型を継承させる。
 }
