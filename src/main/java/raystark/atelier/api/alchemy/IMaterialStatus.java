90cd63bf223e3e62200942fa7b77657cab1ddd96
RayStarkMC <raystarkmc@gmail.com>
2019-04-28 21:35:10 +0900

    錬金素材の型を表すインターフェース追加

---
+++ src/main/java/raystark/atelier/api/alchemy/IMaterialStatus.java
@@ -0,0 +1,9 @@
+package raystark.atelier.api.alchemy;
+
+
+/**
+ * 錬金素材の性能を表すインターフェース
+ */
+public interface IMaterialStatus extends Quality, ElementOwner {
+    // TODO 潜在能力について型を継承させる。
+}
--- src/main/java/raystark/atelier/api/material/IMaterialCategory.java
+++ src/main/java/raystark/atelier/api/material/IMaterialCategory.java
@@ -2,8 +2,7 @@ package raystark.atelier.api.material;
 
 /**
  * 錬金素材のカテゴリを表すインターフェース.
- *
- * 他Modがカテゴリ追加する可能性を考慮してインターフェースを定義
  */
 public interface IMaterialCategory {
+    //他Modがカテゴリ追加する可能性を考慮してインターフェースを定義
 }
