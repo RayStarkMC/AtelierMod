16dde2a4b4be505e1ba37094737c659d33bc0001
RayStarkMC <raystarkmc@gmail.com>
2019-06-12 07:20:39 +0900

    リファクタリング レジストリ初期化回り修正

--- src/main/java/raystark/atelier/common/registry/AtelierRegistry.java
+++ src/main/java/raystark/atelier/common/registry/AtelierRegistry.java
@@ -5,7 +5,7 @@ import net.minecraft.item.Item;
 import raystark.atelier.api.registry.IMaterialRegistry;
 
 public class AtelierRegistry extends AbstractAtelierRegistry<Item, Block> {
-    IMaterialRegistry<Item, Block> materialRegistry;
+    private IMaterialRegistry<Item, Block> materialRegistry;
 
     public AtelierRegistry() {
         materialRegistry = new MaterialRegistry();
