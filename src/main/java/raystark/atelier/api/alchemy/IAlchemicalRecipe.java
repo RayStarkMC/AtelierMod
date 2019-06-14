4cf27e386bf8e5d8a8339a3dcab9c41a62c79281
RayStarkMC <raystarkmc@gmail.com>
2019-06-14 14:35:42 +0900

    レシピAPI作成開始

--- src/main/java/raystark/atelier/api/alchemy/IAlchemicalRecipe.java
+++ src/main/java/raystark/atelier/api/alchemy/IAlchemicalRecipe.java
@@ -2,28 +2,32 @@ package raystark.atelier.api.alchemy;
 
 import raystark.atelier.api.alchemy.potential.IPotentialAbility;
 import raystark.atelier.api.alchemy.status.ElementOwner;
-import raystark.atelier.api.alchemy.status.IMaterialStatus;
-import raystark.atelier.api.alchemy.status.PotentialAbilityOwner;
 
 import java.util.List;
 
 /**
  * 錬金レシピを表すインターフェース.
  *
- * <p>レシピは材料のアイテム指定
+ * <p>レシピは材料としてアイテムそのものを指定できるほか、アイテムの属すカテゴリを指定することが出来る。
+ * 材料としてカテゴリが指定された場合、そのカテゴリに属す任意のアイテムをレシピの材料として用いることが出来る。
  *
- * @param <I> アイテムの型
- * @param <B> ブロックの型
  * @param <S> アイテムスタックの型
  */
-public interface IAlchemicalRecipe<I, B, S> {
+public interface IAlchemicalRecipe<S> {
 
     /**
-     * このレシピを用いてクラフトを実行します。
+     * このレシピの要求物のリストを返します。
+     *
+     * @return このレシピの要求物の不変リスト
+     */
+    List<IAlchemicalRecipeRequirement<S>> getRequirements();
+
+    /**
+     * 属性値と潜在能力から完成品のアイテムスタックを生成します。
      *
      * @param elements レシピに適応する属性値
      * @param abilities アイテムに適応する潜在能力
-     * @return 性能決定後の完成品
+     * @return 性能決定後の完成品のアイテムスタック
      */
     S getProduct(ElementOwner elements, List<IPotentialAbility> abilities);
 }
---
+++ src/main/java/raystark/atelier/api/alchemy/IAlchemicalRecipeRequirement.java
@@ -0,0 +1,17 @@
+package raystark.atelier.api.alchemy;
+
+/**
+ * 錬金術レシピの要求物を表すインターフェース。
+ *
+ * @param <S> アイテムスタック
+ */
+public interface IAlchemicalRecipeRequirement<S> {
+
+    /**
+     * 引数に渡されたアイテムスタックが材料と利用できるか調べます。
+     *
+     * @param input アイテムスタック
+     * @return アイテムがレシピの材料として使用できる場合true
+     */
+    boolean isAppliciableAsMaterial(S input);
+}
