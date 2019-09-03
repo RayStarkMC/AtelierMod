af27bb2167e14a4bb6ffb564553f4aa764047902
RayStark <raystarkmc@gmail.com>
2019-09-03 19:27:44 +0900

    リファクタリング(途中)

--- src/main/java/raystark/atelier/common/item/AtelierItems.java
+++ src/main/java/raystark/atelier/common/item/AtelierItems.java
@@ -1,15 +1,24 @@
 package raystark.atelier.common.item;
 
+import cpw.mods.fml.common.registry.GameRegistry;
 import net.minecraft.item.Item;
 
+import java.util.LinkedList;
+import java.util.List;
+
 public final class AtelierItems {
+    private static List<Item> items;
+
     public static Item samplePickaxe;
     public static Item bandage;
 
     private AtelierItems() {}
 
     public static void init() {
+        items = new LinkedList<>();
         samplePickaxe = new SamplePickaxe();
         bandage = new Bandage();
+
+        items.forEach(item -> GameRegistry.registerItem(item, ""));
     }
 }
--- src/main/java/raystark/atelier/common/item/SamplePickaxe.java
+++ src/main/java/raystark/atelier/common/item/SamplePickaxe.java
@@ -3,6 +3,7 @@ package raystark.atelier.common.item;
 import net.minecraft.block.Block;
 import net.minecraft.creativetab.CreativeTabs;
 import net.minecraft.entity.EntityLivingBase;
+import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import net.minecraft.world.World;
@@ -12,6 +13,7 @@ import raystark.atelier.api.alchemy.effect.IEffectMiningLevel;
 import raystark.atelier.api.util.ToolClasses;
 import raystark.atelier.common.registry.EffectRegistry;
 import raystark.atelier.common.util.AtelierModUtil;
+import scala.reflect.internal.Trees;
 
 import java.util.List;
 import java.util.Objects;
@@ -73,5 +75,29 @@ public class SamplePickaxe extends ItemProductBase implements ItemAlchemicalProd
     @Override
     public float getDigSpeed(ItemStack itemstack, Block block, int metadata) {
         return Objects.equals(block.getHarvestTool(metadata), ToolClasses.PICKAXE.value()) ? 8.0f : 1.0f;
+    }
+
+    @Override
+    public boolean onItemUse(ItemStack p_77648_1_, EntityPlayer p_77648_2_, World p_77648_3_, int p_77648_4_, int p_77648_5_, int p_77648_6_, int p_77648_7_, float p_77648_8_, float p_77648_9_, float p_77648_10_) {
+        boolean ret = super.onItemUse(p_77648_1_, p_77648_2_, p_77648_3_, p_77648_4_, p_77648_5_, p_77648_6_, p_77648_7_, p_77648_8_, p_77648_9_, p_77648_10_);
+        System.out.println("onItemUse: " + ret);
+        new Throwable().printStackTrace();
+
+        return false;
+    }
+
+    @Override
+    public boolean onItemUseFirst(ItemStack stack, EntityPlayer player, World world, int x, int y, int z, int side, float hitX, float hitY, float hitZ) {
+        boolean ret = super.onItemUseFirst(stack, player, world, x, y, z, side, hitX, hitY, hitZ);
+        System.out.println("ontItemUseFirst: " + ret);
+        new Throwable().printStackTrace();
+        return true;
+    }
+
+    @Override
+    public ItemStack onItemRightClick(ItemStack p_77659_1_, World p_77659_2_, EntityPlayer p_77659_3_) {
+        System.out.println("onItemRightClick");
+        new Throwable().printStackTrace();
+        return super.onItemRightClick(p_77659_1_, p_77659_2_, p_77659_3_);
     }
 }
