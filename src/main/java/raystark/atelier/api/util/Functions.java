f0dca8481086f7ba83afa44ede551006f8d45f41
RayStarkMC <raystarkmc@gmail.com>
2020-01-28 21:26:20 +0900

    Functions修正 不要なメソッドを消去 asStream追加

--- src/main/java/raystark/atelier/api/util/Functions.java
+++ src/main/java/raystark/atelier/api/util/Functions.java
@@ -1,25 +1,20 @@
 package raystark.atelier.api.util;
 
-import java.util.function.IntPredicate;
-import java.util.function.Predicate;
+import java.util.*;
+import java.util.stream.Stream;
 
 public final class Functions {
     private Functions() {}
 
     /**
-     * {@code predicate != null ? predicate : (T e) -> false}となる述語を返します。
+     * optと同一要素を持つStreamを返します。
      *
-     * <p>このメソッドはラムダ式やメソッド参照によって渡されたpredicateに対してand、or、negateを適用するために使用されます。
-     *
-     * @param predicate 述語
-     * @param <T> 述語の入力の型
-     * @return 引数の述語
+     * @param opt Optionalのインスタンス
+     * @param <T> Optionalの要素型
+     * @return optと同一要素を持つStreamStream
+     * @throws NullPointerException optがnullの場合
      */
-    public static <T> Predicate<T> predicate(Predicate<T> predicate) {
-        return predicate == null ? e -> false : predicate;
-    }
-
-    public static IntPredicate intPredicate(IntPredicate predicate) {
-        return predicate == null ? e -> false : predicate;
+    public static <T> Stream<T> asStream(@SuppressWarnings("OptionalUsedAsFieldOrParameterType") Optional<T> opt) {
+        return Objects.requireNonNull(opt).map(Stream::of).orElseGet(Stream::empty);
     }
 }
